3afeab8eb88b530c52b1d7d70db79d0aab73c0a5
luancortez <luancortezdev@gmail.com>
2023-02-21 19:57:00 -0300

    feat: :sparkles: Componente Header criado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+
+export default function Header() {
+  return (
+    <Box paddingTop={'20px'}>
+      <Typography variant='h1' fontSize='40px' textAlign='initial' color={'#29EBAA'}>
+        RESUMINDO CAPÍTULOS
+      </Typography>
+    </Box >
+  )
+}
