381db7ba185eb407d42b729e67e9a076fd360cac
luancortez <luancortezdev@gmail.com>
2023-02-22 19:37:52 -0300

    feat: :sparkles: Alguns paths adicionados ao componente Routes: formulario, livros e login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+import { Livros } from '../pages/Livros'
 import Login from '../pages/Login'
-import PaginaPrincipal from '../pages/PaginaPrincipal'
+import Formulario from '../pages/Formulario'
+import { Home } from '../pages/Home'
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/pagina-principal' element={<PaginaPrincipal />} />
+      <Route path='/' element={<NavBar />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/formulario' element={<Formulario />} />
+        <Route path='/livros' element={<Livros />} />
+      </Route>
+      <Route path='/login' element={<Login />} />
     </Routes>
   )
 }
