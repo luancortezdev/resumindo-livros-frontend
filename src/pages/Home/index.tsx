6869a31cc6daff4530b2b3ddcad732cddcdd785e
luancortez <luancortezdev@gmail.com>
2023-02-22 19:35:26 -0300

    feat: :sparkles: Página Home criada

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi amet odit doloribus et aut ipsam dolor, id fuga. Itaque veritatis ducimus nam ipsam! Debitis minima consequatur facilis obcaecati ipsam officiis?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi amet odit doloribus et aut ipsam dolor, id fuga. Itaque veritatis ducimus nam ipsam! Debitis minima consequatur facilis obcaecati ipsam officiis?</p>
+    </div>
+  )
+}
