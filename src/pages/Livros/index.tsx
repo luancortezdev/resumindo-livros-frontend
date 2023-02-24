49faa3d38adbb6ccdf98eab2d0d8770bd0000307
luancortez <luancortezdev@gmail.com>
2023-02-23 23:18:05 -0300

    feat: :sparkles: Detalhes table cell adicionada e livros mockados

--- src/pages/Livros/index.tsx
+++ src/pages/Livros/index.tsx
@@ -2,6 +2,20 @@ import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, 
 import { Container } from '@mui/system'
 
 export const Livros = () => {
+
+  const livros = [
+    {
+      id: 1,
+      titulo: "O Senhor dos Anéis",
+      autor: "J. R. R. Tolkien"
+    },
+    {
+      id: 2,
+      titulo: "Cartas de um diabo a seu aprendiz",
+      autor: "C. S. Lewis"
+    }
+  ]
+
   return (
     <Container maxWidth="lg" sx={{ mt: 1 }}>
       <TableContainer component={Paper}>
@@ -15,6 +29,9 @@ export const Livros = () => {
                 Autor
               </TableCell>
               <TableCell>
+                Detalhes
+              </TableCell>
+              <TableCell>
                 Editar
               </TableCell>
               <TableCell>
@@ -23,22 +40,27 @@ export const Livros = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>
-                Livro
-              </TableCell>
-              <TableCell>
-                Autor
-              </TableCell>
-              <TableCell>
-                {/* onClick={() => navigate(`/livros/${livro.id}`)} */}
-                <Button sx={{ paddingLeft: '0px' }} color="success">editar</Button>
-              </TableCell>
-              <TableCell>
-                {/* color="error" onClick={() => excluir(livro)} */}
-                <Button sx={{ paddingLeft: '0px' }} color="error">excluir</Button>
-              </TableCell>
-            </TableRow>
+            {livros.map((livro, id) => (
+              <TableRow>
+                <TableCell>
+                  {livro.titulo}
+                </TableCell>
+                <TableCell>
+                  {livro.autor}
+                </TableCell>
+                <TableCell>
+                  <Button sx={{ paddingLeft: '0px' }}>Detalhes</Button>
+                </TableCell>
+                <TableCell>
+                  {/* onClick={() => navigate(`/livros/${livro.id}`)} */}
+                  <Button sx={{ paddingLeft: '0px' }} color="success">editar</Button>
+                </TableCell>
+                <TableCell>
+                  {/* color="error" onClick={() => excluir(livro)} */}
+                  <Button sx={{ paddingLeft: '0px' }} color="error">excluir</Button>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
