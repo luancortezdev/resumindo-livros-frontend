f3d20225bfbadb2d39cafdd742cf09727b26b99f
luancortez <luancortezdev@gmail.com>
2023-02-23 23:12:56 -0300

    feat: :lipstick: Links com ícones do GitHub e LinkedIn adicionados

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,20 @@
-import { Box, Typography } from "@mui/material"
-import { Link } from "react-router-dom"
+import { Box, Link, Typography } from "@mui/material"
+import { Link as RouterLink } from 'react-router-dom'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { LinkedIn } from "@mui/icons-material";
+
 
 export const Footer = () => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Typography variant='h4' fontSize='20px' color={'#ffffff'}>
-        Desenvolvido por <Link to='https://github.com/luancortezdev'>Luan Cortez</Link>
+    <Box component="footer" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Typography component="label" variant='overline' >
+        Desenvolvido por Luan Cortez
       </Typography>
+      <Box marginLeft={2}>
+        <Link color="#000000" sx={{ textDecoration: 'none' }} component={RouterLink} target="_blank" to='https://github.com/luancortezdev'><GitHubIcon /></Link>
+        <Link color="#000000" sx={{ textDecoration: 'none' }} component={RouterLink} target="_blank" to='https://www.linkedin.com/in/luancortezdev/'><LinkedIn /></Link>
+
+      </Box>
     </Box >
   )
 }
