3ce571a4e1d8c17e220a69d41fcf65d12d8b809c
luancortez <luancortezdev@gmail.com>
2023-02-23 23:14:18 -0300

    feat: :sparkles: Botão com rota de direcionamento adicionado

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,16 +1,21 @@
+import { useTheme } from '@emotion/react'
 import { AppBar, Button, Link } from '@mui/material'
 import { Box } from '@mui/system'
 import { Link as RouterLink, Outlet } from 'react-router-dom'
 
 export default function NavBar() {
+  const theme = useTheme()
   return (
     <Box component="header">
-      <AppBar position='static' component="nav" sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: "5%" }}>
+      <AppBar position='static' component="nav" sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: "50px" }}>
         <Link component={RouterLink} to="/">
           <Button sx={{ color: 'white' }}>Home</Button>
         </Link>
         <Link component={RouterLink} to="/livros">
           <Button sx={{ color: 'white' }}>Livros</Button>
+        </Link>
+        <Link component={RouterLink} to="/livros/novo">
+          <Button sx={{ color: 'white' }}>Novo Livro</Button>
         </Link>
       </AppBar>
       <Outlet />
