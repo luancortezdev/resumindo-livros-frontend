6bd8f72651f413e5b817398ab01f5da7c3e0af5c
luancortez <luancortezdev@gmail.com>
2023-02-23 23:16:25 -0300

    feat: :sparkles: Componentes Main e Footer importados e utilizados

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { Footer } from '../../components/Footer'
+import { Main } from '../../components/Main'
 
 export const Home = () => {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi amet odit doloribus et aut ipsam dolor, id fuga. Itaque veritatis ducimus nam ipsam! Debitis minima consequatur facilis obcaecati ipsam officiis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi amet odit doloribus et aut ipsam dolor, id fuga. Itaque veritatis ducimus nam ipsam! Debitis minima consequatur facilis obcaecati ipsam officiis?</p>
-    </div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Typography component="h1" variant="h4" sx={{ height: '5vh' }}>Resumindo Livros</Typography>
+      <Box sx={{ height: "85vh" }}>
+        <Main />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '5vh' }}>
+        <Footer />
+      </Box>
+    </Box>
   )
 }
